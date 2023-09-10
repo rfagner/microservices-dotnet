cc946bb0ee66bbb860b52ebdc51b2e9addaca5fb
Fagner Santos <uprogramador@gmail.com>
2023-09-10 17:47:45 -0300

    criando o consumidor | implementando o BackgroundService

---
+++ src/ItemService/RabbitMqClient/RabbitMqSubscriber.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+
+namespace ItemService.RabbitMqClient
+{
+    public class RabbitMqSubscriber : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _nomeDaFila;
+        private readonly IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMqSubscriber(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMqHost"],
+                Port = Int32.Parse(_configuration["RabbitMqPort"]),
+            }.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _nomeDaFila = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(queue: _nomeDaFila, exchange: "trigger", routingKey: "");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
